659258dfb7ff8011dac7d235712faf19e20cea97
Gimms11 <mijael.gara@gmail.com>
2024-11-20 17:37:34 -0500

    201124

--- inkmad-games_the-cursed-mansion/nodes/Enemies/Enemy 1/enemy_1.gd
+++ inkmad-games_the-cursed-mansion/nodes/Enemies/Enemy 1/enemy_1.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-var speed = 5
+var speed = 100
 
 
 @export var player: Node2D
@@ -12,7 +12,8 @@ func _ready() -> void:
 
 func _physics_process(_delta: float) -> void:
 	
-	var dir = to_local(nav_agent.get_next_path_position().normalized())
+	#var dir = to_local(nav_agent.get_next_path_position().normalized())
+	var dir = self.global_position.direction_to(player.global_position)
 	velocity = dir * speed
 	
 	move_and_slide()
