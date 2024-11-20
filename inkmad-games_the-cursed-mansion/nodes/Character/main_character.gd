85e55d831eeed41c85214e266c67e8d298562f2d
Gimms11 <mijael.gara@gmail.com>
2024-11-20 18:30:46 -0500

    Linterna-Persecución (Albert)

--- inkmad-games_the-cursed-mansion/nodes/Character/main_character.gd
+++ inkmad-games_the-cursed-mansion/nodes/Character/main_character.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 const SPEED =150
 
-
+@onready var luz:= $PointLight2D as PointLight2D
 
 func _physics_process(delta):
 	var dir: Vector2 =Input.get_vector("izquierda","derecha","arriba","abajo")
 	var position = global_position
+	
+	if Input.is_action_just_pressed("light"):
+		luz.visible = !luz.visible
+	
 	if dir:
 		if dir.x != 0:
 			if dir.x > 0:
