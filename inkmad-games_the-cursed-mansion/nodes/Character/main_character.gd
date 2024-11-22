00c6a80de9492acf2143d5b09bbd88a5a09c59db
Gimms11 <mijael.gara@gmail.com>
2024-11-21 22:57:11 -0500

    Terreno (Danipza)

--- inkmad-games_the-cursed-mansion/nodes/Character/main_character.gd
+++ inkmad-games_the-cursed-mansion/nodes/Character/main_character.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 const SPEED =150
 
+@onready var walking_sound = $AudioWalk
+
 @onready var luz:= $PointLight2D as PointLight2D
 
 func _physics_process(delta):
@@ -11,6 +13,10 @@ func _physics_process(delta):
 		luz.visible = !luz.visible
 	
 	if dir:
+		# Reproducir sonido si no está ya en reproducción
+		if not walking_sound.playing:
+			walking_sound.play()
+			
 		if dir.x != 0:
 			if dir.x > 0:
 				if dir.y > 0 or dir.y == 0:
@@ -32,7 +38,9 @@ func _physics_process(delta):
 				$"AnimNiña".play("walk_up") 
 				pass   
 	else:
-		
+		# Detener el sonido si el personaje está quieto
+		if walking_sound.playing:
+			walking_sound.stop()
 		$"AnimNiña".play("Idle")
 		
 		pass
