e80124322091e5d09d35a62f1ef19f3d8f447038
Gimms11 <mijael.gara@gmail.com>
2024-11-22 01:18:00 -0500

    Documentación (Albert)

--- inkmad-games_the-cursed-mansion/nodes/Enemies/Enemy 1/enemy_1.gd
+++ inkmad-games_the-cursed-mansion/nodes/Enemies/Enemy 1/enemy_1.gd
@@ -11,23 +11,27 @@ var dir = global_position
 var p = 1
 
 func _ready() -> void:
-	
+	#Crea la ruta hacia el personaje
 	makepaht()
 
 func _physics_process(_delta: float) -> void:
 	
 	#var dir = to_local(nav_agent.get_next_path_position().normalized())
 	
+	#Detecta que la linterna este prendida y que este en el área de visión
 	if player.luz.visible and player in $Area2D.get_overlapping_bodies():
 		makepaht()
+		#Reproduce animación de correr
 		$AnimEnemy.play("Walk")
 		speed = 300
 	else:
 		speed = 0
+		#Reproduce animación de dezcanso
 		$AnimEnemy.play("Idle")
 	
 	velocity = dir * speed
 	
+	#Gira al personaje dependiendo su dirección en x
 	if (dir.x < 0):
 		$AnimEnemy.scale.x = -2
 	else:
