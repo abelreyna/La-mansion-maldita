c739f81e578451ae2d61e534a9c594a5279b50f7
Gimms11 <mijael.gara@gmail.com>
2024-11-22 01:13:47 -0500

    Detección de linterna//giro (Albert)

---
+++ inkmad-games_the-cursed-mansion/Sensor_1/sensor.gd
@@ -0,0 +1,8 @@
+extends Area2D
+class_name Sensor
+
+var target : CollisionShape2D
+var coliciones = []
+
+var target_distance
+var target_direction
--- inkmad-games_the-cursed-mansion/nodes/Enemies/Enemy 1/enemy_1.gd
+++ inkmad-games_the-cursed-mansion/nodes/Enemies/Enemy 1/enemy_1.gd
@@ -8,20 +8,30 @@ var speed = 100
 @onready var nav_agent:= $NavigationAgent2D as NavigationAgent2D
 
 var dir = global_position
+var p = 1
 
 func _ready() -> void:
+	
 	makepaht()
 
 func _physics_process(_delta: float) -> void:
 	
 	#var dir = to_local(nav_agent.get_next_path_position().normalized())
-	if player.luz.visible==true:
+	
+	if player.luz.visible and player in $Area2D.get_overlapping_bodies():
 		makepaht()
+		$AnimEnemy.play("Walk")
 		speed = 300
 	else:
 		speed = 0
+		$AnimEnemy.play("Idle")
 	
 	velocity = dir * speed
+	
+	if (dir.x < 0):
+		$AnimEnemy.scale.x = -2
+	else:
+		$AnimEnemy.scale.x = 2
 	
 	move_and_slide()
 
